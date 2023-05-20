f245f7b51e73dce8acdc569624370bcd56bbd783
ks1619 <ks1619@ic.ac.uk>
2023-05-20 23:36:49 +0100

    Moved logvar to tensor

--- stable-diffusion/ldm/models/diffusion/ddpm.py
+++ stable-diffusion/ldm/models/diffusion/ddpm.py
@@ -1027,7 +1027,9 @@ class LatentDiffusion(DDPM):
         loss_simple = self.get_loss(model_output, target, mean=False).mean([1, 2, 3])
         loss_dict.update({f'{prefix}/loss_simple': loss_simple.mean()})
 
-        logvar_t = self.logvar[t].to(self.device)
+        self.logvar = self.logvar.to(self.device)
+        logvar_t = self.logvar[t]
+        # logvar_t = self.logvar[t].to(self.device)
         loss = loss_simple / torch.exp(logvar_t) + logvar_t
         # loss = loss_simple / torch.exp(self.logvar) + self.logvar
         if self.learn_logvar:
